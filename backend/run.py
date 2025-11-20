5c6b3209423c6c4cd9340c0ec6498cc33074d4cb
nelly <nelly2gmail.com>
2025-11-20 13:27:36 +0100

    add frontend

--- backend/run.py
+++ backend/run.py
@@ -1,7 +1,7 @@
 # run.py
 
 import uvicorn
-import config
+import config as config
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host=config.APP_HOST, port=config.APP_PORT, reload=config.APP_DEBUG)
